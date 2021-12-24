1ca5df67057b4ec300486f94eccc921e7a061564
Kevin Workman <kevinaworkman@gmail.com>
2021-12-24 15:29:27 -0800

    for loops

---
+++ week-06-for-loops/README.md
@@ -0,0 +1,34 @@
+# Week 6: For Loops
+
+This week, you'll be displaying information about a speech that you choose.
+
+The speech can be funny, serious, or educational. You can write your own speech,
+or you can use a speech from your favorite book, movie, or TV show. You could
+also use your favorite poem or song lyrics.
+
+The only requirement about the speech is that it must contain at least 100
+words.
+
+After you choose a speech, modify `index.html` and `script.js` to output
+information about your speech. In addition to the existing example section that
+shows words with more than 5 characters, your page should include these
+sections:
+
+- Words with less than 4 characters
+- Words that start with the same letter as your name (if none, feel free to
+  choose a different letter)
+- The longest word, or the shortest word
+- Every 3rd word
+- Words that aren't the 10 most common words
+
+There are ways to do some of this without for loops, but the goal of this
+project is to practice using for loops. When you're done, your code should
+contain at least 5 for loops, not counting the existing example.
+
+Modifying the CSS is optional, so make sure your JavaScript works first.
+
+Write your code one small piece at a time. Don't try to get all 5 sections
+working at once! Write the code for a single section, and then test that single
+section in your browser. Check your browser's developer tools console for
+errors. When you have a single section working, then move on to a second
+section. Repeat that process until you have at least 5 sections.
---
+++ week-06-for-loops/script.js
@@ -0,0 +1,44 @@
+// Replace this speeh with your own.
+// Notice the `` backticks, which let you split your string into multiple lines.
+const speech = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+    est laborum.`;
+
+// Remove punctuation from the speech. You might have to modify this if your
+// speech contains other punctuation.
+const speechPunctuationRemoved = speech.replace(',', '').replace('.', '');
+
+// Use a regular expression to split the speech into individual words. You
+// shouldn't need to change this, unless you want to split on characters other
+// than whitespace.
+const wordsArray = speechPunctuationRemoved.split(/\s+/);
+
+// Displays words that have more than 5 characters.
+function displayLongWords() {
+  const longWordsElement = document.getElementById('long-words');
+
+  // Loop over every word in the array.
+  for(let i = 0; i < wordsArray.length; i++) {
+    const word = wordsArray[i];
+    // If the word has more than 5 characters, display it in the page.
+    if(word.length > 5) {
+      const wordElement = document.createElement('li');
+      wordElement.innerText = word;
+      longWordsElement.appendChild(wordElement);
+    }
+  }
+}
+
+// TODO: Define your own functions here!
+
+function displaySpeechStats() {
+  document.getElementById('speech').innerText = speech;
+
+  displayLongWords();
+
+  // TODO: Call your functions here!
+}
