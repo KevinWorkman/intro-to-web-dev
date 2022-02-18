fdb71893e4b9e2eb533f89d98715089bb9803b7c
jmmayes012 <98127454+jmmayes012@users.noreply.github.com>
2022-02-18 15:00:26 -0500

    week 4

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -13,8 +13,60 @@ function fillOutputs() {
   // Set the text of the input element
   adjectiveOneOutput.innerText = adjectiveOne;
 
+  const nounOneInput = document.getElementById('noun-one-input');
+  // Get the text from the input element
+  const nounOne = nounOneInput.value;
+
+  // Get the adjective output element
+  const nounOneOutput = document.getElementById('noun-one-output');
+  // Set the text of the input element
+  nounOneOutput.innerText = nounOne;
+
+  const verbOneInput = document.getElementById('verb-one-input');
+  // Get the text from the input element
+  const verbOne = verbOneInput.value;
+
+  // Get the adjective output element
+  const verbOneOutput = document.getElementById('verb-one-output');
+  // Set the text of the input element
+  verbOneOutput.innerText = verbOne;
+
+  const nounTwoInput = document.getElementById('noun-two-input');
+  // Get the text from the input element
+  const nounTwo = nounTwoInput.value;
+
+  // Get the adjective output element
+  const nounTwoOutput = document.getElementById('noun-two-output');
+  // Set the text of the input element
+  nounTwoOutput.innerText = nounTwo;
+
+  const adverbOneInput = document.getElementById('adverb-one-input');
+  // Get the text from the input element
+  const adverbOne = adverbOneInput.value;
+
+  // Get the adjective output element
+  const adverbOneOutput = document.getElementById('adverb-one-output');
+  // Set the text of the input element
+  adverbOneOutput.innerText = adverbOne;
+
+
+
+
+
   // Get the output container element
   const outputDiv = document.getElementById('output');
   // Make the output container element visible
   outputDiv.style.display = 'block';
+
+
+
+
+
+
+
+
+
+
+
+
 }
